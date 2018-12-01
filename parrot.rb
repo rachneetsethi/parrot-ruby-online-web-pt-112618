2dc2f35738d706991be7b12ccc49642052f88fa8
Rachneet <rachneetsethi@gmail.com>
2018-12-01 19:20:42 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (sound, lips = "Squawk")
+  puts "The sound is #{sound}.Their lips #{lips}!"
+end 
+parrot ("wisp")
+return parrot 
+
+
